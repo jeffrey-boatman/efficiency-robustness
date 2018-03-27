9a48d89b3f990f9c846651db154be961d2ae0c76
jeffrey-boatman <boat0036@umn.edu>
2018-03-27 11:00:38 -0500

    Update sim.R

--- sim.R
+++ sim.R
@@ -5,6 +5,7 @@ n.mc <- 1000                       # number of Monte Carlo Iterations
 mc.cores <- 24                     # number of cores to use
 sample.sizes <- c(250, 500, 1000)  # sample sies assuming no kown compliers
 num.tries <- 4                     # number of sets of starting values for em algorithm
+n.boot <- 50
 # ---------------------------------------------------------------------------------------
 
 library(parallel)
@@ -48,7 +49,9 @@ mu <- c(0, 2, 2)
 
 
 sim <- function(seed, coef, type = c("Interaction", "Quadratic"), num.tries, 
-  include.known = FALSE, mis.spec = TRUE, write) {
+  include.known = FALSE, mis.spec = TRUE, 
+  auc = c("high", "low"),
+  write) {
   # coef: coefficient for interaction or quadratic term
   # num.tries: number of sets of starting values for em algorithm. Must be at least 2
   # include.known: whether to include known compliers
@@ -57,6 +60,7 @@ sim <- function(seed, coef, type = c("Interaction", "Quadratic"), num.tries,
   #  will write results to outfile, which must be globally defined.
 
   type <- match.arg(type)
+  auc <- match.arg(auc)
 
   if(missing(mis.spec)) stop("missing mis.spec arg")
   if(missing(write)) stop("missing write arg")
@@ -98,8 +102,12 @@ sim <- function(seed, coef, type = c("Interaction", "Quadratic"), num.tries,
     gamma <- c(-9.3, -0.8, 0.7)    
     bio.lp <- c(cbind(1, comp, y) %*% gamma)
     biost <- rnorm(n)
-    b.sd <- 0.40 
-    bio <- bio.lp + b.sd * biost
+    # b.sd <- 0.40 
+    b.sd.vals <- list(
+      high = 0.4400557,
+      low = 0.6694395
+    ) 
+    bio <- bio.lp + b.sd.vals[[auc]] * biost
 
     estimators.l <- estimate(xx1 = x1, 
       xx2 = x2, 
@@ -116,8 +124,59 @@ sim <- function(seed, coef, type = c("Interaction", "Quadratic"), num.tries,
       gamma = gamma)
     (estimators <- estimators.l$estimators)
     (em.fail <- as.numeric(estimators.l$em.fail))
-    rr <- data.frame(t(c(n, seed, type, coef, 
-      estimators - true.mu, 
+
+    # --- bootstrap --- #
+    # n.boot <- 10 # now defined globally
+    if(coef < 2 | !mis.spec) {
+      boot.dist <- array(dim = c(n.boot, length(estimators)))
+      n.unknown <- n - n.known
+      for(b in seq_len(n.boot)) {
+        set.seed(72576 + seed * n.boot + b)
+        known.samp <- sort(sample(seq_len(n.known), 
+          size = n.known, replace = TRUE))
+        unknown.samp <- sort(sample(n.known + seq_len(n.unknown), 
+          size = n.unknown, 
+          replace = TRUE))
+        samp <- c(known.samp, unknown.samp)
+        boot.estimators.l <- estimate(xx1 = x1[samp], 
+          xx2 = x2[samp], 
+          xx3 = x3[samp],
+          ccomp = comp[samp], 
+          bbio = bio[samp], 
+          yy = y[samp],
+          kknown = known[samp],
+          mmis.spec = mis.spec,
+          coef = coef,
+          num.tries = num.tries, 
+          type = type,
+          y.coefs = y.coefs,
+          gamma = gamma)
+        (boot.estimators <- boot.estimators.l$estimators)
+        boot.dist[b, ] <- boot.estimators
+      }
+
+      boot.sd <- apply(boot.dist, 2, sd, na.rm = TRUE)
+
+      conf.level <- 0.95
+      (z <- qnorm((1 + conf.level) / 2))
+      (boot.conf.lims <- array(
+        data = estimators + cbind(- z * boot.sd, z * boot.sd),
+        dim = c(length(estimators), 2),
+        dimnames = list(base.names, c("2.5%", "97.5%"))
+      ))
+
+      covers <- boot.conf.lims[, 1] < true.mu & 
+        true.mu < boot.conf.lims[, 2]
+      covers <- as.numeric(covers)
+    } else{
+      boot.sd <- covers <- rep(NA, length(estimators))
+    }
+    
+
+    rr <- data.frame(t(c(n, seed, type, auc, coef, 
+      estimators - true.mu,
+      boot.sd,
+      covers, 
       known = include.known,
       mis.spec = mis.spec,
       em.fail)))
@@ -138,11 +197,25 @@ base.names <- c("ipw", "reg", "aipw",
   "cure", "cure.plus",
   "em.reg", "wreg",
   "acure")
-col.names <- c("n", "seed", "type", "coef", 
-  paste0(base.names, ".bias"),
+col.names <- c("n", "seed", "type", "auc", "coef", 
+  paste0(base.names, ".error"),
+  paste0(base.names, ".stderr"),
+  paste0(base.names, ".covers"),
   "known",
   "mis.spec",
   "em.fail")
+
+# for debugging
+# debug(sim)
+# n.boot <- 2
+# sim(seed = 1,
+  # coef = 2,
+  # type = "Interaction",
+  # num.tries = 4,
+  # include.known = TRUE,
+  # mis.spec = FALSE,
+  # auc = "high",
+  # write = FALSE)
 
 sims <- ((j * n.mc / n.s) - (n.mc / n.s - 1)):(j * n.mc / n.s)
 
@@ -154,34 +227,56 @@ write.table(t(col.names),
   col.names = FALSE, 
   row.names = FALSE)
 
-# coef == 0
-for(include.known in c(TRUE, FALSE)) {
-  out <- mclapply(sims,
-    sim,
-    mc.cores = mc.cores,
-    coef = 0,
-    type = "Interaction",
-    num.tries = num.tries,
-    include.known = include.known,
-    mis.spec = FALSE,
-    write = TRUE)
-}
+args.list <- list(
+  # table 2 ----------------
+  list(coef = 0, 
+    type = "Interaction", 
+    mis.spec = FALSE, 
+    auc = "low"),
+  list(coef = 0, 
+    type = "Interaction",  
+    mis.spec = FALSE, 
+    auc = "high"),
+  # table 3 ----------------
+  list(coef = 1, 
+    type = "Interaction",  
+    mis.spec = FALSE, 
+    auc = "high"),
+  list(coef = 1, 
+    type = "Interaction",  
+    mis.spec = TRUE, 
+    auc = "high"),
+  # table 3 ----------------
+  list(coef = 1, 
+    type = "Quadratic",  
+    mis.spec = FALSE, 
+    auc = "high"),
+  list(coef = 1, 
+    type = "Quadratic",  
+    mis.spec = TRUE, 
+    auc = "high")
+)
 
-# coef != 0
-for(include.known in c(TRUE, FALSE)) {
-  for(coef in c(1, 2)) {
-    for(type in c("Interaction", "Quadratic")) {
-      for(mis.spec in c(TRUE, FALSE)) {
-        out <- mclapply(sims,
-          sim,
-          mc.cores = mc.cores,
-          coef = coef,
-          type = type,
-          num.tries = num.tries,
-          include.known = include.known,
-          mis.spec = mis.spec,
-          write = TRUE)
-      }
-    }
-  }
+for(args in args.list) {
+  cat(
+    "Running with:\n",
+      "mc.cores = ", mc.cores, "\n",
+      "coef = ", args$coef, "\n",
+      "type =", args$type, "\n",
+      "num.tries =", num.tries, "\n",
+      "include.known =", TRUE, "\n",
+      "mis.spec = ", args$mis.spec, "\n",
+      "auc =", args$auc, "\n",
+      "write =", TRUE, "\n"
+  )
+  out <- mclapply(sims, 
+    sim,
+    mc.cores = mc.cores, 
+    coef = args$coef, 
+    type = args$type, 
+    num.tries = num.tries, 
+    include.known = TRUE, 
+    mis.spec = args$mis.spec,
+    auc = args$auc, 
+    write = TRUE)
 }
