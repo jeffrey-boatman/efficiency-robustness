7253a0591454f9f60311829c7f3e97644151139a
jeffrey-boatman <boat0036@umn.edu>
2018-03-27 11:03:12 -0500

    Update summary-tables.R

--- summary-tables.R
+++ summary-tables.R
@@ -3,228 +3,197 @@
 # ----------------------------------------
 options(digits = 3)
 library(xtable)
-library(dplyr)
+library(tidyverse)
 
 
-# functions -------------------------------------------------
-prop.na <- function(x) mean(is.na(x))
-
-
-rm.outliers <- function(x) {
-  mult <- 9 / 4
-  iqr <- IQR(x, na.rm = TRUE)
-  m <- median(x, na.rm = TRUE)
-  lower <- m - (mult * iqr)
-  upper <- m + (mult * iqr)
-  x[(x < lower) | (upper < x)] <- NA
-  x
-}
-
-rmdup <- function(x, dup) {
-  x[dup] <- NA
-  x 
-}
-
-
-sum.na <- function(x) sum(is.na(x))
-
-last <- function(x) x[length(x)]
-
-make.xtable <- function(df, add.consistency.col = FALSE,
-  no.coef = FALSE, clean = FALSE) {
-  ident.names <- c("n", "type", "coef")
-  ident.pos <- match(ident.names, names(df))
-  bias.pos <- grep("bias", names(df))
-  bias.names <- names(df)[bias.pos]
-  df <- df[, c(ident.pos, bias.pos)]
-  new.ident.pos <- match(ident.names, names(df))
-
-  iqr <- aggregate(df[bias.names], 
-    df[ident.names],
-    IQR,
-    na.rm = TRUE)
-  medians <- aggregate(df[bias.names], 
-    df[ident.names],
-    median,
-    na.rm = TRUE)
-  iqr.long <- reshape(iqr,
-    idvar = ident.names,
-    times = setdiff(names(iqr), ident.names),
-    timevar = "Estimator",
-    varying = list(setdiff(names(iqr), ident.names)),
-    v.names = "iqr",
-    direction = "long")
-  row.names(iqr.long) <- NULL
-  medians.long <- reshape(medians,
-    idvar = ident.names,
-    times = setdiff(names(medians), ident.names),
-    timevar = "Estimator",
-    varying = list(setdiff(names(medians), ident.names)),
-    v.names = "median",
-    direction = "long")
-  row.names(medians.long) <- NULL
-  df$dummy.seed <- seq_len(nrow(df))
-  df.long <- reshape(df,
-    idvar = c(ident.names, "dummy.seed"),
-    times = setdiff(bias.names, ident.names),
-    timevar = "Estimator",
-    varying = list(setdiff(bias.names, ident.names)),
-    v.names = "bias",
-    direction = "long")
-  row.names(df.long) <- NULL
-  temp.df <- left_join(df.long, medians.long)
-  temp.df <- left_join(temp.df, iqr.long)
-  temp.df <- mutate(temp.df,
-    lower = median - 9 / 4 * iqr,
-    upper = median + 9 / 4 * iqr,
-    outlier = bias < lower | upper < bias,
-    bias = ifelse(outlier, NA, bias))
-  temp.df$median <- temp.df$iqr <- temp.df$lower <- 
-    temp.df$upper <- temp.df$outlier <- NULL
-  temp.df <- reshape(temp.df,
-    idvar = c("dummy.seed", ident.names),
-    timevar = "Estimator",
-    direction = "wide")
-  temp.df$dummy.seed <- NULL
-  df$dummy.seed <- NULL
-  names(temp.df) <- names(df)
-
-  num.outliers <- aggregate(temp.df[, -new.ident.pos], 
-    temp.df[ident.names], 
-    sum.na)
-  num.outliers <- reshape(num.outliers,
-    idvar = ident.names,
-    times = setdiff(names(num.outliers), ident.names),
-    timevar = "Estimator",
-    varying = list(setdiff(names(num.outliers), ident.names)),
-    v.names = "n.outliers",
-    direction = "long")
-
-
-  if(clean) {
-    df <- temp.df
-  }
-  means = aggregate(df[, -new.ident.pos],
-    df[ident.names],
-    mean,
-    na.rm = TRUE)
-  means <- reshape(means, 
-    idvar = ident.names, 
-    times = setdiff(names(df), ident.names), 
-    timevar = "Estimator",
-    varying = list(setdiff(names(df), ident.names)),
-    v.names = "bias",
-    direction = "long")
-  ses <- aggregate(df[, -new.ident.pos],
-    df[ident.names],
-    sd,
-    na.rm = TRUE)
-  ses <- reshape(ses,
-    idvar = ident.names,
-    times = setdiff(names(df), ident.names),
-    timevar = "Estimator",
-    varying = list(setdiff(names(df), ident.names)),
-    v.names = "mcsd",
-    direction = "long")
-  tt <- left_join(means, ses)
-  tt <- mutate(tt, mse = bias ^ 2 + mcsd ^ 2)
-  tt <- left_join(tt, num.outliers)
-  (ff <- reshape(tt,
-    idvar = c("type", "coef", "Estimator"),
-    v.names = c(c("bias", "mcsd", "mse", "n.outliers")),
-    timevar = "n",
-    direction = "wide"))
-  ff <- ff[order(ff$type, ff$coef), ]
-  rownames(ff) <- NULL
-
-  ff$coef <- rmdup(ff$coef, duplicated(ff[c("type", "coef")]))
-  ff$type <- rmdup(ff$type, duplicated(ff$type))
-  rr <- c("IPW", "REG", "A-IPW", "CURE", "CURE+", 
-    "EM-REG", "W-REG", "A-CURE")
-  ff$Estimator <- rep(rr, nrow(ff) / length(rr))
-  samp.sizes <- unique(df$n)
-  nums.pos <- Map(grep, as.list(as.character(samp.sizes)), list(names(ff)))
-
-  # add extra spaces to allow multiline commands in latex
-  foo <- list()
-  foo[seq_len(length(nums.pos)) * 2 - 1] <- nums.pos
-  bar <- lapply(foo, function(x) ff[, x])
-  bar[seq(2, length(bar) - 1, 2)] <- NA
-  if(no.coef) {
-    ff <- cbind(ff[, 3], do.call(cbind, bar))
-  } else {
-    ff <- cbind(ff[, 1:3], do.call(cbind, bar))
-  }
-  base.digits <- c(3, 3, 3, 0, 0)
-  if(add.consistency.col) {
-    cons <- c("Y", "N", "Y", "Y", "N", "N", "N", "Y")
-    ff$consistent <- rep(cons, nrow(ff) / length(rr))
-    dd <- c(0, 0, 0, 0, rep(base.digits, length(unique(df$n)) - 1), rev(rev(base.digits)[-1]), 0)
-  } else {
-    dd <- c(0, 0, 0, 0, rep(base.digits, length(unique(df$n)) - 1), rev(rev(base.digits)[-1]))
-  } 
-  if (no.coef) {
-    dd <- dd[-c(1, 2)]
-  }
-  xff <- xtable(ff, digits = dd)
-  print(xff,
-    include.rownames = FALSE,
-    include.colnames = FALSE,
-    only.contents = TRUE)
-}
-
-
-ttl <- lapply(paste0("outfiles/out_", 1:5, ".txt"), 
+ttl <- lapply(paste0("outfiles/", dir("outfiles")), 
   read.table, 
   header = TRUE,
   as.is = TRUE)
 tt <- do.call(rbind, ttl)
 
-# ------------------------------------ #
-# --- results with known compliers --- #
-# ------------------------------------ #
-nocoef.out <- subset(tt, type == "Interaction" & coef == 0 & !mis.spec & known)
-nomis.out <- subset(tt, coef != 0 & known & !mis.spec)
-mis.out <- subset(tt, coef != 0 & known & mis.spec)
+tt <- as.tibble(tt)
 
 
-with(nocoef.out, ftable(n, type, coef))
-with(nomis.out, ftable(type, n, coef))
-with(mis.out, ftable(type, n, coef))
+# bias
+bb <- tt %>%
+  select(-ends_with("stderr"), -ends_with("covers")) %>%
+  gather(ends_with("error"), key = "estimator", value = "error") %>%
+  group_by(n, type, coef, known, mis.spec, estimator) %>%
+  mutate(
+    median = median(error, na.rm = TRUE),
+    iqr = diff(quantile(error, c(0.25, 0.75), na.rm = TRUE)),
+    lower = median - 9 / 4 * iqr,
+    upper = median + 9 / 4 * iqr,
+    outlier = as.numeric(error < lower | error > upper)) %>%
+  ungroup() %>%
+  separate(estimator, into = c("estimator", "junk"), 
+    sep = ".error",
+    convert = TRUE) %>%
+  select(-junk)
 
-nrow(subset(nocoef.out, em.fail == 1)) # 0
-nrow(subset(nomis.out, em.fail == 1))  # 0
-nrow(subset(mis.out, em.fail == 1))    # 0
+# stderr 
+ss <- tt %>%
+  select(-ends_with("error"), -ends_with("covers")) %>%
+  gather(ends_with("stderr"), key = "estimator", value = "stderr") %>%
+  separate(estimator, into = c("estimator", "junk"), 
+    sep = ".stderr",
+    convert = TRUE) %>%
+  select(-junk)
 
-# tables with outliers removed ------------------------
-make.xtable(nocoef.out, no.coef = TRUE, clean = TRUE)
-make.xtable(nomis.out, clean = TRUE)
-make.xtable(mis.out, clean = TRUE, add.consistency.col = FALSE)
+# coverage prob
+cc <- tt %>%
+  select(-ends_with("error"), -ends_with("stderr")) %>%
+  gather(ends_with("covers"), key = "estimator", value = "cp") %>%
+  separate(estimator, into = c("estimator", "junk"), 
+    sep = ".covers",
+    convert = TRUE) %>%
+  select(-junk)
+
+ll <- bb %>%
+  left_join(ss) %>%
+  left_join(cc)
+
+grouped <- ll %>%
+  group_by(n, type, auc, coef, known, mis.spec, estimator)
+
+grouped.noout <- filter(grouped, outlier == 0)
+##grouped.noout <- grouped
+
+grouped.noout <- grouped.noout %>%
+  group_by(n, type, auc, coef, known, mis.spec, estimator)
+
+summ <- grouped.noout %>%
+  summarize(
+    bias = mean(error, na.rm = TRUE),
+    mcsd = sd(error, na.rm = TRUE),
+    mse = mean(error ^ 2, na.rm = TRUE),
+    cp = mean(cp, na.rm = TRUE)
+  ) 
 
 
-# Tables for supplementary materials/appendix
-make.xtable(nocoef.out, no.coef = TRUE, clean = FALSE)
-make.xtable(nomis.out, clean = FALSE)
-make.xtable(mis.out, clean = FALSE, add.consistency.col = FALSE)
+bias <- summ %>%
+  select(-mcsd, -mse, -cp) %>%
+  spread(key = n, value = bias, sep = ".") %>%
+  rename(
+    bias.n.275 = n.275,
+    bias.n.550 = n.550,
+    bias.n.1100 = n.1100
+  )
 
-# --------------------------------------- #
-# --- results without known compliers --- #
-# --------------------------------------- #
-nocoef.out <- subset(tt, type == "Interaction" & coef == 0 & !mis.spec & !known)
-nomis.out <- subset(tt, coef != 0 & !known & !mis.spec)
-mis.out <- subset(tt, coef != 0 & !known & mis.spec)
+mcsd <- summ %>%
+  select(-bias, -mse, -cp) %>%
+  spread(key = n, value = mcsd, sep = ".") %>%
+  rename(
+    mcsd.n.275 = n.275,
+    mcsd.n.550 = n.550,
+    mcsd.n.1100 = n.1100
+  )
+
+mse <- summ %>%
+  select(-mcsd, -bias, -cp) %>%
+  spread(key = n, value = mse, sep = ".") %>%
+  rename(
+    mse.n.275 = n.275,
+    mse.n.550 = n.550,
+    mse.n.1100 = n.1100
+  )
+
+cp <- summ %>%
+  select(-mcsd, -bias, -mse) %>%
+  spread(key = n, value = cp, sep = ".") %>%
+  rename(
+    cp.n.275 = n.275,
+    cp.n.550 = n.550,
+    cp.n.1100 = n.1100
+  )
+
+summ.no <- grouped %>%
+  summarize(
+    num.outliers = sum(outlier, na.rm = TRUE)
+  )
+
+no <- summ.no %>%
+  spread(key = n, value = num.outliers, sep = ".") %>%
+  rename(
+    num.outliers.n.275 = n.275,
+    num.outliers.n.550 = n.550,
+    num.outliers.n.1100 = n.1100
+  )
 
 
-with(nocoef.out, ftable(n, type, coef))
-with(nomis.out, ftable(type, n, coef))
-with(mis.out, ftable(type, n, coef))
+est.order <- c("ipw", "reg", "aipw", "cure", 
+  "cure.plus", "em.reg", "wreg", "acure")
+est.names <- c("IPW", "REG", "A-IPW", "CURE", "CURE+", "EM-REG", 
+  "W-REG", "A-CURE")
 
-nrow(subset(nocoef.out, em.fail == 1)) # 0
-nrow(subset(nomis.out, em.fail == 1))  # 0
-nrow(subset(mis.out, em.fail == 1))    # 0
+mm <- bias %>%
+  left_join(mcsd) %>%
+  left_join(mse) %>%
+  left_join(cp) %>%
+  left_join(no) 
 
-# tables with outliers removed ------------------------
-make.xtable(nocoef.out, no.coef = TRUE, clean = TRUE)
-make.xtable(nomis.out, clean = TRUE)
-make.xtable(mis.out, clean = TRUE, add.consistency.col = FALSE)
+cbind(1:ncol(mm), names(mm))
 
+mm <- mm %>%
+  select(c(1, 3:5, 2, 6, 7, 10, 13, 16, 19, 8, 11, 14, 17, 20, 9, 12, 15, 18, 21)) %>%
+  mutate(
+    ord = match(estimator, est.order),
+    estimator = est.names[ord])
+
+mm <- mm %>%
+  ungroup()
+
+digs <- c(0, 0, rep(c(rep(3, 4), 0), 3))
+h.after <- c(3,8)
+#,11,16)
+
+# --------------- #
+# --- Table 2 --- #
+# --------------- #
+
+t2 <- mm %>% 
+  filter(coef == 0) %>%
+  arrange(auc, ord) %>%
+  select(-c(1:5), -ord)
+
+print(xtable(t2, digits = digs), 
+  file = "xtables/table2.tex",
+  hline.after = h.after,
+  only.contents = TRUE,
+  include.colnames = FALSE,
+  include.rownames = FALSE)
+
+# --------------- #
+# --- Table 3 --- #
+# ---_----------- #
+
+t3 <- mm %>% 
+  filter(type == "Interaction",
+         coef == 1) %>%
+  arrange(mis.spec, ord) %>%
+  select(-(1:5), -ord)
+
+print(xtable(t3, digits = digs), 
+      file = "xtables/table3.tex",
+      hline.after = h.after,
+      only.contents = TRUE,
+      include.colnames = FALSE,
+      include.rownames = FALSE)
+
+# --------------- #
+# --- Table 4 --- #
+# --------------- #
+
+t4 <- mm %>% 
+  filter(type == "Quadratic",
+         coef == 1) %>%
+  arrange(mis.spec, ord) %>%
+  select(-(1:5), -ord)
+
+print(xtable(t4, digits = digs), 
+      file = "xtables/table4.tex",
+      hline.after = h.after,
+      only.contents = TRUE,
+      include.colnames = FALSE,
+      include.rownames = FALSE)
